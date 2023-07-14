86d9dd524f66c3d71b2f914f4f05e0747ede0738
Ariwibowogithub <arie35411@gmai.com>
2023-07-14 17:50:24 +0700

    first commit

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,6 +11,9 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    
     }
 }
+
